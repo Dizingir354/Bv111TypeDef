c53530b7e149606e225489138c2763557985705f
Dizingir354 <Dizingir355@gmail.com>
2022-06-21 20:12:08 +0300

    createProject

---
+++ Release/Release/Source.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+using si_t short int;
+
+typedef unsugned long long ull_l;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="a">первое число</param>
+/// <param name="b">второе число</param>
+/// <returns></returns>
+int sum(int a, int b)
+{
+	return a + b;
+}
+/// <summary>
+/// 
+/// </summary>
+/// <param name="a">первое число</param>
+/// <param name="b">второе число</param>
+/// <returns></returns>
+float sum(float a, float b)
+{
+	return a - b;
+}
+ull_t sum(ull_t a, ull_t b)
+{
+	return a - b;
+}
+
+
+
+unsigned long long num1 = 10l;
+ull_t num2 = 100l;
+
+void main()
+{
+	for (size_t i = 0; i < length; i++)
+	{
+
+	}
+	setlocale(LC_ALL, "Russian");
+	cout << "Начало программы" << endl;
+	system("pause");
+	string s;
+
+	sum(1, 2);
+	sum(1.0f, 2.5f);
+}
